18ad2d49c7eab625f0fd96a99c265dde7cd9c059
Emiel <emielverhoef@hotmail.com>
2022-08-26 02:52:19 +0200

    yml added

--- app/main.py
+++ app/main.py
@@ -1,27 +1,3 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from app.model.model import mod_predict
-
-app = FastAPI()
-
-
-class TextIn(BaseModel):
-    text: str
-    model: str
-    labels: dict
-
-
-class PredictionOutput(BaseModel):
-    prediction: str
-
-
-@app.get("/")
-def home():
-    return {"test": "ok"}
-
-
-@app.post("/predict", response_model=PredictionOutput)
-def predict(payload: TextIn):
-    prediction = mod_predict(payload.model, payload.text)
-    print(payload.labels)
-    return {'prediction':payload.labels[str(prediction)]}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4651b77e4952bb06f13f5ee84023321ca6e5a18fc896ebbe0e25e9aa41232df
+size 556
